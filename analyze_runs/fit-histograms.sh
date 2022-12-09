8aed8361183686b50f011b28e29ba0aafeef5084
Matteo Vicenzi <matteo.vicenzi7@gmail.com>
2022-12-09 15:29:46 -0600

    single file execution to avoid I/O errors

--- analyze_runs/fit-histograms.sh
+++ analyze_runs/fit-histograms.sh
@@ -1,2 +1,2 @@
 export file=$1
-fitPulseDistribution -i $file -d calibrationdb/
+fitPulseDistribution -i $file -d calibrationdb/ -v 1
--- analyze_runs/make-histograms_original.sh
+++ analyze_runs/make-histograms_original.sh
@@ -1,8 +1,9 @@
 export run=$1
 export limit=20
 export list=input/files-run${run}.list
-export histdir="./histograms_splitted/"
+export histdir="./histograms/"
 export fcl="bkgphotons-calibration.fcl"
+
 
 
 # This part creates a files list
@@ -28,23 +29,13 @@ echo "Project has ${njobs} files"
 
 
 # This part does the fit
-#if test -f "${histdir}/pulseDistributionHist_run${run}.root"; then 
+if test -f "${histdir}/pulseDistributionHist_run${run}.root"; then 
 	
-#	echo "file ${histdir}/pulseDistributionHist_run${run}.root already exists!"
-#	echo "File will be replaced!"
+	echo "file ${histdir}/pulseDistributionHist_run${run}.root already exists!"
+	echo "File will be replaced!"
 	#rm ${histdir}/pulseDistributionHist_run${run}.root
-#fi 
+fi 
 
-ID=0
-cat $list | while read line || [[ -n $line ]];
-do
-
-echo $line
-
-#lar -c ${fcl} -S input/files-run${run}.list -T ${histdir}/pulseDistributionHist2_run${run}.root
-lar -c ${fcl} -s $line -T ${histdir}/pulseDistributionHist_${ID}_run${run}.root
-
-((ID++))
-done
+lar -c ${fcl} -S input/files-run${run}.list -T ${histdir}/pulseDistributionHist_run${run}.root
 
 echo "ALL DONE!"
